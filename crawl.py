b39b9cba22cfb9c8f1c4ae8e6f25ae659b1e2b45
medicalissue <medicalissue@naver.com>
2024-06-03 23:26:07 +0900

    3

--- crawl.py
+++ crawl.py
@@ -1,8 +1,8 @@
-import os
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
 
+# 크롤링에 필요한 xpath 주소들
 idXpath = '//*[@id="login_user_id"]'
 pwXpath = '//*[@id="login_user_password"]'
 loginXpath = '//*[@id="form1"]/div[4]'
@@ -15,14 +15,16 @@ sendXpath = '//*[@id="img_btn"]'
 toType = ('조식', '중식', '석식')
 menuXpath = '//*[@id="root"]/div/div/div[2]/div/div/div[*]'
 
-
+# 오늘까지 제출해야하는 과제물 크롤링
 def getDashboard(ID, PW):
+    # 웹드라이버 선언
     options = webdriver.ChromeOptions()
-    options.add_argument("headless")
+    options.add_argument("headless") # headless 옵션을 붙여 크롬 창이 백그라운드에서 동작
     driver = webdriver.Chrome(options=options)
-    driver.implicitly_wait(10)
+    driver.implicitly_wait(10) # 로딩될때까지 기다려주는 시간 10초
     driver.get(url='https://eclass3.cau.ac.kr')
 
+    # 아이디와 비밀번호, 로그인버튼 객체를 찾고 id,pw를 입력한 후 로그인 버튼 클릭
     idBox = driver.find_element(By.XPATH, idXpath)
     pwBox = driver.find_element(By.XPATH, pwXpath)
     loginBtn = driver.find_element(By.XPATH, loginXpath)
@@ -30,62 +32,105 @@ def getDashboard(ID, PW):
     pwBox.send_keys(PW)
     loginBtn.click()
 
+    # 로그인 실패시 예외처리
     if driver.current_url == "https://canvas.cau.ac.kr/xn-sso/gw-cb.php":
         return "Fail"
 
+    # 일자별로 정리된 창을 불러오기 위해 설정을 바꾸는 코드. 
     driver.find_element(By.XPATH, optionsXpath).click()
     driver.find_element(By.XPATH, setXpath).click()
-    # driver.get(url='https://eclass3.cau.ac.kr')
 
+    # 오늘까지 TODO인것들을 dashboardTodo에 저장.
     dashBoardTodo = driver.find_elements(By.XPATH, todoXpath)[0].text.split("\n")
     
     retTodo = list()
+    # 확인용 print. 실제 gui에는 표시되지 않음.
     print(dashBoardTodo)
+    # 없을 경우
     if "Nothing Planned" in dashBoardTodo[0]:
         retTodo.append("Nothing")
+    # 있을 경우:과제물 이름 뒤에 붙는 "is not marked as done"을 이용해 과제 이름만 파싱.
     else:
         for i in range(len(dashBoardTodo)):
             if "is not marked as done." in dashBoardTodo[i]:
                 retTodo.append(dashBoardTodo[i][3:-23] + "\n")
+    
+    # todo.puang 에 해야하는 과제물 이름을 씀.
     path = "./resources/todo.puang"
-    if os.path.isfile(path):
-        os.remove(path)
     f = open(path, 'w')
     f.writelines(retTodo)
     f.close()
 
 def getMenu():
+    #메뉴 갱신 전까지 임시로 띄울 메시지
+    with open("./resources/menu.puang", "w") as f:
+        f.write("메뉴 갱신중.. 잠시만 기다려주세요!")
+
+    #크롤링할때 오류 발생시 갱신 실패 메시지를 띄우기 위한 try-except
     try:
+        #웹드라이버 선언
         options = webdriver.ChromeOptions()
         options.add_argument("headless")
         driver = webdriver.Chrome(options=options)
         driver.implicitly_wait(10)
-        driver.get(url='https://chat.cau.ac.kr/v2/index.html')
-        time.sleep(0.5)
+
+        driver.get(url='https://chat.cau.ac.kr/v2/index.html') # 중앙대 챗봇 링크(로그인 필요 x)
+        time.sleep(0.5) # 챗봇 로딩 기다려주는 시간
+    
+        # 입력, 전송 element 찾기
         chat = driver.find_element(By.XPATH, chatXpath)
         send = driver.find_element(By.XPATH, sendXpath)
-        menu = list()
+
+        # 조식, 중식, 석식 챗봇에게 물어보기
         for i in range(3):
-            time.sleep(0.5)
+            time.sleep(0.5) #챗봇 응답 대기 시간
             chat.send_keys(f"서울캠퍼스 {toType[i]} 메뉴")
             send.click()
-        time.sleep(1)
+        time.sleep(1) # 여유 시간
+
+        # 모든 chat 기록 menu에 일단 저장
         menu = list(map(lambda x: x.text, driver.find_elements(By.CLASS_NAME, "bubble_area")))
-        # print(*menu, sep="\n===================\n")
+        
+        # 조식, 중식, 석식 메뉴만 파싱하여 menu.puang에 쓰기
         f = open("./resources/menu.puang", "w")
         for i in range(len(menu)):
-            if "메뉴입니다." in menu[i]:
+            if "메뉴입니다." in menu[i]: # "메뉴입니다." <= 필요없는 데이터를 구분짓는 구문
                 temp = menu[i].split("\n")
                 for j in range(len(temp)):
                     if "메뉴입니다." in temp[j]:
                         temp = temp[j:-1]
                         break
-                f.write("\n".join(temp) + "\n\n")
+                f.write("\n".join(temp).strip("\n") + "\n\n")
         f.close()
-        return
     except:
-        return "Fail"
+        with open("./resources/menu.puang", "w") as f:
+            f.write("학식 갱신 실패..") # 오류 발생시 갱신 실패 메시지 적기
 
-# if __name__ == "__main__":
+def getFortune():
+    options = webdriver.ChromeOptions()
+    options.add_argument("headless")
+    driver = webdriver.Chrome(options=options)
+    driver.implicitly_wait(10)
+
+    driver.get(url="https://fortune.nate.com/contents/freeunse/freeunseframe.nate?freeUnseId=today04")
+    driver.switch_to.frame("contentFrame")
+
+    fortune = list()
+
+    for i in range(1, 24, 2):
+        #tee > tbody > tr > td:nth-child(1) > a
+        #tee > tbody > tr > td:nth-child(23) > a
+        btn = driver.find_element(By.XPATH, f"""//*[@id="tee"]/tbody/tr/td[{i}]""")
+        print(btn)
+        btn.click()
+        time.sleep(0.5)
+        context = driver.find_element(By.XPATH, """//*[@id="con_box"]""").text
+        fortune.append(context)
+    
+    for i in range(12):
+        with open(f"./resources/fortune/{i}.puang", "w") as f:
+            f.write(fortune[i])
+if __name__ == "__main__":
+    getFortune()
     # getMenu()
     # print(getDashboard(ID, PW))
--- gui.py
+++ gui.py
@@ -4,22 +4,28 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtGui import QIcon
 from PyQt5.QtCore import *
 import crawl
-from datetime import datetime
 
-IDPW_PATH = "./settings/idpw.puang"
-todoToday = list()
+IDPW_PATH = "./settings/idpw.puang" # id, pw 저장된 주소 미리 선언
+todoToday = list() # 비동기 함수를 수월하게 동작시켜주기 위한 오늘까지 해야할 과제물들을 담는 전역변수 미리 선언
+dt = QDate()
 
 class Todo(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.todayDate = datetime.now().date()
+        self.todayDate = dt.currentDate()
+        self.updater = Update(self)
+        self.menuUpdater = updateMenu(self)
+        self.fortuneUpdater = updateFortune(self)
         self.initUI()
         self.cb = QLabel(self)
         self.cb.setText("Loading...")
         self.cb.move(20, 40)
         self.cb.show()
+        self.combo.currentIndexChanged.connect(self.updateFortune)
         self.updater.doneSignal.connect(self.updateCheckbox)
         self.updater.start()
+        self.menuUpdater.start()
+        self.fortuneUpdater.start()
         self.timer = QTimer(self)
         self.timer.start(1000)
         self.timer.timeout.connect(self.updateDate)
@@ -29,22 +35,43 @@ class Todo(QMainWindow):
         self.setWindowIcon(QIcon('./resources/puang.png'))
         path = os.path.join(os.path.dirname(sys.modules[__name__].__file__), './resources/puang.png')
         app.setWindowIcon(QIcon(path))
+        
+        birthday = ["양력 1월 20일~2월 18일", "양력 2월 19일~3월 20일", "양력 3월 21일~4월 19일", "양력 4월 20일~5월 20일", "양력 5월 21일~6월 21일", "양력 6월 22일~7월 22일", "양력 7월 23일~8월 22일", "양력 8월 23일~9월 23일", "양력 9월 24일~10월 22일", "양력 10월 23일~11월 22일", "양력 11월 23일~12월 24일", "양력 12월 25일~1월 19일"]
+        self.combo = QComboBox()
+        self.combo.addItems(birthday)
+        self.combo.setWindowTitle("생일 입력!")
+        self.ftsh = QLabel()
+        self.ftsh.setWindowTitle("오늘의 운세")
+        self.ftsh.move(20, 20)
+        self.ftsh.setText("")
+        self.ftsh.show()
+
+        currentTime = QDateTime.currentDateTime()
+        self.endDate = QDateEdit()
+        self.endDate.setDateTime(currentTime)
+        self.endDate.setCalendarPopup(True)
 
         self.date = QLabel()
         self.updateDate()
         self.statusBar().addWidget(self.date)
 
-        setAction = QAction(QIcon('./resources/cau.png'), 'Set ID/PW', self)
+        setAction = QAction(QIcon('./resources/cau.png'), 'ID/PW 설정', self)
         setAction.triggered.connect(self.setLogin)
         setAction.setShortcut('Ctrl+W')
 
-        menuAction = QAction(QIcon('./resources/bob.png'), 'Menu', self)
+        menuAction = QAction(QIcon('./resources/bob.png'), '오늘의 학식', self)
         menuAction.triggered.connect(self.showMenu)
+
+        untilAction = QAction(QIcon('./resources/until.png'), '종강까지 몇일?', self)
+        untilAction.triggered.connect(self.setEndDate)
+
+        fortuneAction = QAction(QIcon('./resources/clover.webp'), '오늘의 운세', self)
+        fortuneAction.triggered.connect(self.showFortune)
+
         infoAction = QAction(QIcon('./resources/puang.png'), 'Info', self)
 
-        refreshAction = QAction(QIcon('./resources/refresh.png'), 'Refresh', self)
+        refreshAction = QAction(QIcon('./resources/refresh.png'), '새로고침', self)
         refreshAction.setShortcut('Ctrl+R')
-        self.updater = Update(self)
         refreshAction.triggered.connect(self.updater.start)
 
         menubar = self.menuBar()
@@ -58,6 +85,8 @@ class Todo(QMainWindow):
 
         info = menubar.addMenu('Info')
         info.addAction(menuAction)
+        info.addAction(untilAction)
+        info.addAction(fortuneAction)
         info.addAction(infoAction)
 
         self.setGeometry(300, 300, 300, 200)
@@ -88,11 +117,16 @@ class Todo(QMainWindow):
         self.updater.start()
 
     def updateDate(self):
-        if self.todayDate != datetime.now().date():
+        if self.todayDate != dt.currentDate():
             # print(self.todayDate)
             self.updater.start()
-        self.date.setText(datetime.today().strftime("%Y-%m-%d %H:%M:%S"))
-        self.todayDate = datetime.now().date()
+            self.menuUpdater.start()
+            self.fortuneUpdater.start()
+
+        # print(self.todayDate.daysTo(self.endDate.date()))
+        # print(self.endDate.text())
+        self.date.setText(QDateTime.currentDateTime().toString("yyyy-MM-dd") + f" 종강까지 D-{self.todayDate.daysTo(self.endDate.date())}")
+        self.todayDate = dt.currentDate()
 
     def updateCheckbox(self):
         global todoToday
@@ -104,12 +138,23 @@ class Todo(QMainWindow):
         self.cb.show()
 
     def showMenu(self):
-        if crawl.getMenu() == "Fail":
-            QMessageBox.warning(self, "ERROR", "학식 정보를 받는중 오류가 발생했습니다!")
-            return
         with open('./resources/menu.puang', 'r') as f:
-            menu = f.readlines()
-        QMessageBox.information(self, "학식 정보", "".join(menu).strip())
+            QMessageBox.information(self, "학식 정보", "".join(f.readlines()).strip())
+
+    def setEndDate(self):
+        self.endDate.show()
+
+    def showFortune(self):
+        self.combo.show()
+
+    def updateFortune(self):    
+        i = self.combo.currentIndex()
+        with open(f"./resources/fortune/{i}.puang", "r") as f:
+            ft = f.readlines()
+        self.ftsh.clear()
+        self.ftsh.setText("".join(ft))
+        self.ftsh.adjustSize()
+        self.ftsh.show()
 
 class Update(QThread):
     doneSignal = pyqtSignal()
@@ -137,6 +182,39 @@ class Update(QThread):
             print(todoToday)
 
         self.doneSignal.emit()
+
+class updateMenu(QThread):
+    def __init__(self, parent):
+        super().__init__(parent)
+
+    def run(self):
+        crawl.getMenu()
+
+class updateFortune(QThread):
+    def __init__(self, parent):
+        super().__init__(parent)
+
+    def run(self):
+        crawl.getFortune()
+
+# class fortunePopup(QWidget):
+#     def __init__(self):
+#         QWidget.__init__(self)
+#         self.initUI()
+#         self.combo.currentIndexChanged.connect(self.updateFortune)
+
+#     def initUI(self):
+#         self.setWindowTitle('오늘의 운세!')
+#         birthday = ["양력 1월 20일~2월 18일", "양력 2월 19일~3월 20일", "양력 3월 21일~4월 19일", "양력 4월 20일~5월 20일", "양력 5월 21일~6월 21일", "양력 6월 22일~7월 22일", "양력 7월 23일~8월 22일", "양력 8월 23일~9월 23일", "양력 9월 24일~10월 22일", "양력 10월 23일~11월 22일", "양력 11월 23일~12월 24일", "양력 12월 25일~1월 19일"]
+#         self.combo = QComboBox()
+#         self.combo.addItems(birthday)
+#         self.ftsh = QLabel()
+#         self.ftsh.move(20, 20)
+#         self.ftsh.setText("")
+#         self.ftsh.show()
+#         self.combo.show()
+#         self.setGeometry(300, 300, 300, 200)
+#         self.show()
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
